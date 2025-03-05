63b11002672e73f78aec02832178f268dbe66518
hardfist <yangjianzju@gmail.com>
2025-03-05 17:04:07 +0800

    chore: remove outdated

--- rolldown.config.js
+++ rolldown.config.js
@@ -1,5 +1,11 @@
-module.exports = {
+const {defineConfig} = require('rolldown');
+const path = require('path');
+console.log(path.resolve(__dirname,'./fixtures/input1.js'));
+module.exports = defineConfig({
    input: {
-     main: './fixtures/input1.js'
+     main: path.resolve(__dirname,'./fixtures/input1.js')
+   },
+   output: {
+    dir: path.resolve(__dirname,'dist/rolldown')
    }
-}
+})
